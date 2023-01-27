d3f6564a9edc362ae3f46ada1805db8168d50d70
AC <chenyibo416@gmail.com>
2023-01-27 11:23:15 +0800

    first commit

---
+++ fs-async.js
@@ -0,0 +1,28 @@
+const { readFile, writeFile } = require("fs");
+
+readFile("./context/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  //   console.log(result);
+  const first = result;
+  readFile("./context/second.txt", "utf-8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./context/result-async.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
